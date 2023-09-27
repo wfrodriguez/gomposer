6a7f63ac9a492ad0710123c513549c43860c6bcd
Walter Fabián Rodríguez Salazar <wfabianrs@gmail.com>
2023-09-27 10:01:22 -0500

    hammer(nvim) Actualización de configuración neovim

--- .nvim.lua
+++ .nvim.lua
@@ -2,7 +2,10 @@ function abbr(a, w)
 	vim.cmd("iabbrev " .. a .. " " .. w)
 end
 
+local fn = require("functions")
+
 vim.opt.expandtab = false
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 abbr("ee", ":=")
+fn.nmap("<F5>", ":VimuxRunLastCommand<CR>")
